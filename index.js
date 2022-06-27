2c5917c976d569e73044673b87e2a556fb179652
Joe Scott <jbs198585@gmail.com>
2022-06-27 07:16:20 -0400

    Cookie parser middleware

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.get('/greet', (req, res) => {
-  res.send("HEY THERE!")
-})
+  const { name = 'No-name' } = req.cookies;
+  res.send(`Hey there, ${name}`);
+});
 
 app.get('/setname', (req, res) => {
   res.cookie('name', 'stevie chicks');
+  res.cookie('animal', 'harlequin shrimp');
   res.send('OK SENT YOU A COOKIE!');
-})
+});
 
 app.listen(3000, () => {
   console.log("SERVING!")
