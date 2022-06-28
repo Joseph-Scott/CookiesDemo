0a29d4b645397f4ce2bf371d8828f8fb154630b8
Joe Scott <jbs198585@gmail.com>
2022-06-28 06:17:11 -0400

    Signing cookies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
   const { name = 'No-name' } = req.cookies;
@@ -10,9 +10,20 @@ app.get('/greet', (req, res) => {
 });
 
 app.get('/setname', (req, res) => {
-  res.cookie('name', 'stevie chicks');
+  res.cookie('name', 'henrietta');
   res.cookie('animal', 'harlequin shrimp');
   res.send('OK SENT YOU A COOKIE!');
+});
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true })
+  res.send('OK SIGNED YOUR FRUIT COOKIE!')
+});
+
+app.get('/verifyfruit', (req, res) => {
+  console.log(req.cookies)
+  console.log(req.signedcookies)
+  res.send(req.signedCookies)
 });
 
 app.listen(3000, () => {
